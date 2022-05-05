6ac531a7832b7d5b7c4745ea1aa130f801712275
ralphmensah <ralphmensah123@gmail.com>
2022-05-05 07:53:17 +0000

    init commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow} = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 640,
+    height: 640,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(
+    path.join(__dirname, `/dist/index.html`),
+  );
+  // Open the DevTools.
+ // mainWindow.webContents.openDevTools();
+
+  // mainWindow.on('closed', function () {
+  //   mainWindow = null
+  // })
+}
+
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
+
